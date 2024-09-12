edf1633d0295ee4f28cb66c6d51007e62a3104fb
huytran845~ <huytrit@gmail.com>
2024-09-12 00:21:54 -0700

    Added error handling, authentication, and logging examples.

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
 # rest-api-python
-A basic RESTful API using python and flask for example purposes of a flask app.
+### A example framework of a RESTful API using python and flask.
+
+#### Description
+The purpose of this project was to demonstrate the basic functionalities and features of a RESTful API with the flask library. This api showcases how the API can send information from GET requests, as well as respond appropriately based on the type of request method needed in a backend call. Additionally, POST requests can be sent back to the server and the API can submit data to the server for processing based on the call. Error handling is showcased in the API as well, where information from a GET request isn't availble, the API will respond with an error 404. Basic authentication is demonstrated through a decorator function that can only be accessed with proper credentials. Lastly, logging is a feature through the imported logging library.
+
+#### How to Run and Install
+***This project was made to be demonstrated on the Ubantu environment***
+***Make sure you have python 3+ installed***
+1.Download the restAPI.py file.
+2.Open up a terminal.
+3.Enter the command: "sudo pip install flask".
+4.Run the program with: "python3 restAPI.py".
+
+#### How to Use The Framework
+*Testing root*
+curl http://127.0.0.1:5000/
+
+*Testing basic route and variable selection*
+curl http://127.0.0.1:5000/articles
+curl http://127.0.0.1:5000/articles/123 (Or any value can replace 123, demonstrates that value's selection)
+
+*Testing route provided parameters*
+curl http://127.0.0.1:5000/hello_user (no argument provided)
+curl http://127.0.0.1:5000/hello_user?name=Joe (Joe provided as argument for name value)
+
+*Testing request methods*
+curl -X GET http://127.0.0.1:5000/echo (or just curl http://127.0.0.1:5000/echo because default is GET request)
+curl -X POST http://127.0.0.1:5000/echo
+curl -X PATCH http://127.0.0.1:5000/echo
+curl -X PUT http://127.0.0.1:5000/echo
+curl -X DELETE http://127.0.0.1:5000/echo
+
+*Testing data requests and data headers*
+curl -H "Content-type: application/json" -X POST http://127.0.0.1:5000/messages -d '{"message":"Hello Data"}' (emulates data sent in POST as a JSON)
+
+curl -H "Content-type: application/octet-stream" -X POST http://127.0.0.1:5000/messages --data-binary @message.bin (emulates data sent in POST as a file, which gets written out)
+
+*Viewing response headers*
+curl -i http://127.0.0.1:5000/hello
+
+*Error handling demonstration*
+curl http://127.0.0.1:5000/users/1 (1 can be replaced with any value, and if it's not in valid range, will result in an error message).
+
+*Testing basic authentication*
+curl http://127.0.0.1:5000/secrets (No user information provided, resulting in error message.)
+curl -u "admin:secret" http://127.0.0.1:5000/secrets (If "admin" or "secret" is changed authentication will fail because of invalid credentials, resulting in error message.)
+
+*Logging*
+curl http://127.0.0.1:5000/logging (Generates an "app.log" file where all the logs are directed, different levels indicate the different type of information from the logs.)
--- restAPI.py
+++ restAPI.py
@@ -1,8 +1,13 @@
 #An example frame of a RESTful Flask API
 
-from flask import Flask, url_for, request, json
+from flask import Flask, url_for, request, json, Response, jsonify
+from functools import wraps
+import logging
 
 app = Flask(__name__)
+file_handler = logging.FileHandler('app.log')
+app.logger.addHandler(file_handler)
+app.logger.setLevel(logging.INFO)
 
 @app.route('/')
 def api_root():
@@ -16,8 +21,8 @@ def api_articles():
 def api_article(articleid):
 	return 'You are reading ' + articleid + '\n'
 
-@app.route('/hello') #Example if browser url is /hello, returns Jane Doe. If browser url has /hello?name=variable, it will return the variable instead.
-def api_hello():
+@app.route('/hello_user') #Example if browser url is /hello_user, returns Jane Doe. If browser url has /hello_user?name=variable, it will return the variable instead.
+def api_hello_user():
 	if 'name' in request.args:
 		return 'Hello ' + request.args['name'] + '\n'
 	else:
@@ -50,5 +55,76 @@ def api_message():
 	else:
 		return "Error 415 Unsupported Media Type\n"
 
+@app.route('/hello', methods = ['GET']) #Example of a GET request where data being requested is returned in a JSON format through the API.
+def api_hello():
+    data = {
+        'hello'  : 'world',
+        'number' : 3
+    }
+    js = json.dumps(data)
+
+    resp = Response(js, status=200, mimetype='application/json')
+    resp.headers['Link'] = 'http:/youtube.com'
+
+    return resp
+
+@app.errorhandler(404) #Example of error handling in the API, when something is not found, the flask.jsonify is utilized to create a flask.Response() with the included content header. So we don't need the above Response() manual call.
+def not_found(error=None):
+    message = {
+            'status': 404,
+            'message': 'Not Found: ' + request.url,
+    }
+    resp = jsonify(message)
+    resp.status_code = 404
+
+    return resp
+
+@app.route('/users/<userid>', methods = ['GET']) #This function demonstrates the use of the error handling function, not_found() by having an example sample of data and checking the user values among them.
+def api_users(userid):
+    users = {'1':'john', '2':'steve', '3':'bill'}
+    
+    if userid in users:
+        return jsonify({userid:users[userid]})
+    else:
+        return not_found()
+
+def check_auth(username, password): #check_auth is a function that verifies if the username and password are correct. Currently, it is a skeleton function that only allows the username admin and password secret.
+	return username == 'admin' and password == 'secret'
+
+def authenticate(): #The authenticate function returns a message to the user to authenticate in the situation where authentification fails.
+	message = {'message': "Authenticate.\n"}
+	resp = jsonify(message)
+
+	resp.status_code = 401
+	resp.headers['WWW-Authenticate'] = 'Basic realm="Example"'
+
+	return resp
+
+def requires_auth(f): #Tested with curl -v -u "admin:secret" http://127.0.0.1:5000/secrets which is the authenticated user. This is a decorator that takes the function f.
+	@wraps(f) #This is from the functool library copies the decorator function so that doctools can properly recognize the function without function information being lost. I.E. requires_auth will show instead of decorated.
+	def decorated(*args, **kwargs):
+		auth = request.authorization #If authorization is empty then it becomes None
+		if not auth: 
+			return authenticate()
+
+		elif not check_auth(auth.username, auth.password): #Checks if the inputed information is correct, if not then output same message
+			return authenticate()
+		return f(*args, **kwargs) #Returns with a function call to the original function if successful since layers of authentication have been successfully passed.
+
+	return decorated
+
+@app.route('/secrets')
+@requires_auth
+def api_secrets():
+	return "Successfully accessed hidden information!\n"
+
+@app.route('/logging', methods = ['GET'])
+def api_logging():
+    app.logger.info('informing')
+    app.logger.warning('warning')
+    app.logger.error('This is an example error message!')
+    
+    return "Check your logs\n"
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+	app.run(host='0.0.0.0') #use app.run(debug=True) to run debug mode
